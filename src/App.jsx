31f8935c04e170ca1ba1d76524d8431693ae6c47
deepcodeboy <ayrtonwe13@gmail.com>
2023-04-17 09:54:49 -0300

    segundo commit, agregado de boton para quitar y boton de listar

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ function App() {
   const [pokemons, setPokemons] = useState([])
 
   useEffect(() => {
+    console.log(pokemons)
+  }, [pokemons])
+
+  
     const getPokemons = async () => {
       const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=30&offset=0')
       const listaPokemons = await response.json()
@@ -17,29 +21,34 @@ function App() {
         const poke = await response.json()
 
         return {
-          id: pokemon.id,
+          id: poke.id,
           name: poke.name,
           img: poke.sprites.other["official-artwork"].front_default
         }
       })
       setPokemons(await Promise.all(newPokemons))
     }
-    getPokemons()
-  }, [])
+    
+    const handleBorrar=(nombre)=>{//borra un elemento de la lista
+      setPokemons(pokemons.filter(e=>e.name!=nombre))
+    }
 
   return (
     <>
       <Navbar />
       <div className="App">
         <h1>Pokedex</h1>
-        <div>
+      <button onClick={getPokemons}>Listar Pokemons</button>
+
+        <div className='row'>
           {
             pokemons.map((pokemon, index) => {
               return (
                 <div key={index}>
-                  <img src={pokemon.img} alt={pokemon.name}></img>
+                  <img src={pokemon.img} alt={pokemon.name}></img><br />
+                  <span>#{pokemon.id}</span><br />
                   <p>{pokemon.name}</p>
-                  <span>{pokemon.id}</span>
+                  <button onClick={() => handleBorrar(pokemon.name)}>Quitar</button>
                 </div>
               )
             })
